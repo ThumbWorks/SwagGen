ff03326935e50a5f60094dcab62ce48b14d2021f
Roderic Campbell <roderic@gmail.com>
2020-07-07 09:38:51 -0700

    Update README and do some general cleanup

--- Templates/Vapor/Sources/API.swift
+++ Templates/Vapor/Sources/API.swift
@@ -2,20 +2,23 @@
 
 import Vapor
 
-extension Client {
-    private func genericRequest<T: Content>(method: HTTPMethod, url: URI, headers:  HTTPHeaders, returnType: T.Type, with client: Client) -> EventLoopFuture<T> {
-        return client.send(method, headers: headers, to: url)
-            .flatMapThrowing { clientResponse  in
-                return try clientResponse.content.decode(returnType.self)
-        }
-    }
+public struct {{ options.name }} {
+
+    public static var safeOptionalDecoding = {% if options.safeOptionalDecoding %}true{% else %}false{% endif %}
+    public static var safeArrayDecoding = {% if options.safeArrayDecoding %}true{% else %}false{% endif %}
+    /// Used to encode Dates when uses as string params
+    public static var dateEncodingFormatter = DateFormatter(formatString: "yyyy-MM-dd'T'HH:mm:ssZZZZZ",
+                                                            locale: Locale(identifier: "en_US_POSIX"),
+                                                            calendar: Calendar(identifier: .gregorian))
 }
+
 {% if servers %}
 public enum Server {
-    {% for server in servers %}
+
     {% if info.version %}
     public static let version = "{{ info.version }}"
     {% endif %}
+    {% for server in servers %}
     {% if server.variables %}
     public static func {{ server.name }}({% for variable in server.variables %}{{ variable.name }}: String = "{{ variable.defaultValue }}"{% ifnot forloop.last %}, {% endif %}{% endfor %}) -> String {
         var url = "{{ server.url }}"
