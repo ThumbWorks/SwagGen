282b46b76c3910fc3984f9f4c635ff5306b98c21
Roderic Campbell <roderic@gmail.com>
2020-07-06 17:39:13 -0700

    Implement Basic vapor requests with strongly typed response types

--- Templates/Vapor/Package.swift
+++ Templates/Vapor/Package.swift
@@ -11,15 +11,14 @@ let package = Package(
         .library(name: "{{ options.name }}", targets: ["{{ options.name }}"])
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
         {% for dependency in options.dependencies %}
-        .package(url: "https://github.com/{{ dependency.github }}.git", .exact("{{ dependency.version }}")),
+        .package(url: "https://github.com/{{ dependency.github }}.git", from: "{{ dependency.version }}"),
         {% endfor %}
     ],
     targets: [
         .target(name: "{{ options.name }}", dependencies: [
           {% for dependency in options.dependencies %}
-          "{{ dependency.pod }}",
+          .product(name: "{{ dependency.name }}", package: "{{ dependency.lowerName }}")
           {% endfor %}
         ], path: "Sources")
     ]
