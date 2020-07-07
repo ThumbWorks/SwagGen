b241884da8e7953e97ff9128e9d03852addd18d4
Roderic Campbell <roderic@gmail.com>
2020-07-07 09:39:43 -0700

    Remove test shell scripts

--- generateDeckOfCardsClient.sh
+++
@@ -1,1 +0,0 @@
-swaggen generate https://api.swaggerhub.com/apis/Thumbworks/DeckOfCards/1.0.0#free --template Templates/Vapor  --destination ~/dev/Thumbworks/VaporDeckOfCards/
--- generateEcobeeClient.sh
+++
@@ -1,1 +0,0 @@
-swaggen generate https://api.swaggerhub.com/apis/Thumbworks/Ecobee/0.1#free --template Templates/Vapor  --destination ~/dev/Thumbworks/VaporEcobee/
